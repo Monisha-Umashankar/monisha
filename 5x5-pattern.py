afeaf151381a31bfc9d937f2fd3e9c18da7db2d1
Monisha-Umashankar <151719010+Monisha-Umashankar@users.noreply.github.com>
2024-06-11 15:04:54 +0530

    Add files via upload

---
+++ 5x5-pattern.py
@@ -0,0 +1,17 @@
+for i in range(5):
+    for j in range(5):
+        print('*', end=' ')
+    print()
+
+#include<stdio.h>
+int main(){
+    int a = 10;
+    if(a < 20)
+    {
+        printf("Value of a is less than 20\n");
+    }
+    else{
+        printf("Value is greater than 20\n");
+    }
+}
+    
